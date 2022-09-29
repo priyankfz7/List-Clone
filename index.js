089836d50eb28e04cf88170ad8a1013fd9f3d163
Shibangi Das <shibangid319@gmail.com>
2022-09-30 00:49:07 +0530

    added Product

---
+++ index.js
@@ -0,0 +1,311 @@
+let Product_data = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1578632292335-df3abbb0d586?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZmFzaGlvbiUyMGNsb3RoaW5nfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Marc Jacobs",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "The Mini Tote Bag - Black",
+    price: "$189",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "24S",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1572804013427-4d7ca7268217?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGZhc2hpb24lMjBjbG90aGluZ3xlbnwwfDF8MHx8&auto=format&fit=crop&w=500&q=60",
+    name: "NIKE",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "Air Humara X Jacquemus Ale Brown And Gold Sneakers - Brown",
+    price: "$178.07",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "Cettire",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://images.unsplash.com/flagged/photo-1559502867-c406bd78ff24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGZhc2hpb24lMjBjbG90aGluZ3xlbnwwfDF8MHx8&auto=format&fit=crop&w=500&q=60",
+    name: "Marc Jacobs",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "The Mini Tote Bag - Natural",
+    price: "$189",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "24S",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1576566588028-4147f3842f27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2xvdGhpbmd8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "PRADA",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "Pr 17ws Rectangular-frame Acetate Sunglasses - Black",
+    price: "$366.62 (20% off)",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "Cettire",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1588099768531-a72d4a198538?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2xvdGhpbmd8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "ALEXANDER MCQUEEN",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "Oversized Leather Sneaker - White",
+    price: "$499 (10% off)",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "Gilt",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1554568218-0f1715e72254?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNsb3RoaW5nfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "NAKED WOLFE",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "Spice Faux-leather Knee-thigh Heeled Boots - Black",
+    price: "$325",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "Tessuti UK",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1578681994506-b8f463449011?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNsb3RoaW5nfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "CALVIN KLEIN",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "Curve Box Logo T-shirt - Black",
+    price: "$44",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "Tessuti UK",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "CONVERSE",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "Chuck Taylor All Star Hi-top Sneakers - Black",
+    price: "$65",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "The Hut",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1518049362265-d5b2a6467637?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2hvZXN8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "GUCCI",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "GG Marmont Matelassé Leather Super Mini Bag - Black",
+    price: "$800",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "Mytheresa",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1605408499391-6368c628ef42?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8c2hvZXN8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "The Snapshot Small",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "The Mini Tote Bag - Black",
+    price: "$377.10 $290 (20% off)",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "Cettire",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://cdna.lystit.com/200/250/tr/photos/ruelala/6ba27c8f/balenciaga-Pink-Triple-S-Clear-Sole-Sneaker.jpeg",
+    name: "TBALENCIAGA",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "Triple S Clear Sole Sneaker - Pink",
+    price: "$556(50% off)",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "Rue La La",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1531310197839-ccf54634509e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNob2VzfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "BURBERRY",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "Vintage Check Canvas Sneaker - Natural",
+    price: "$475",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "SSENSE",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1610398752800-146f269dfcc8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aGVlbHN8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "MARC JACOBS",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "Women's Black Leather The Glam Shot Cross-body Bag - Black",
+    price: "$216 (15% off)",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "Cettire",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1582738412120-7f6baeff4da9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGhpZ2glMjBoZWVsfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "GABBANA",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "Calendar-print Poplin Corset - Multicolor",
+    price: "$533",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "Gabbana",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1579727027525-352607d8dc49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGhpZ2glMjBoZWVsfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "GUCCI",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "Dionysus gg Supreme Super Mini Bag - Natural",
+    price: "$726",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "Mytheresa",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1579727027525-352607d8dc49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGhpZ2glMjBoZWVsfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "GUCCI",
+    like: "https://cdn3.iconfinder.com/data/icons/twitter-20/512/166_Heart_Love_Like_Twitter-512.png",
+    des: "Dionysus gg Supreme Super Mini Bag - Natural",
+    price: "$726",
+    link: "https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-link-thin.png",
+    p: "Mytheresa",
+    details:
+      "Get 15% off with 24S on orders over $200 using code FALL15. Valid until Oct. 24, 2022.",
+    p_details: "All sizes are available, the jacket was made on leather",
+  },
+];
+
+//console.log(data);
+
+localStorage.setItem("product_data", JSON.stringify(Product_data));
+
+let container = document.getElementById("container");
+
+function Display(data) {
+  //   container.innerHTML = null;
+  data.forEach(function (ele) {
+    let div = document.createElement("div");
+    div.id = "prod_div";
+
+    let img = document.createElement("img");
+    img.src = ele.image;
+    img.id = "picture";
+
+    let div1 = document.createElement("div");
+    div1.id = "div1";
+    let name = document.createElement("h4");
+    name.innerText = ele.name;
+    name.id = "name";
+    let like = document.createElement("img");
+    like.src = ele.like;
+    like.id = "like";
+    div1.append(name, like);
+
+    let des = document.createElement("p");
+    des.innerText = ele.des;
+    let price = document.createElement("h4");
+    price.innerHTML = ele.price;
+
+    let div2 = document.createElement("div");
+    div2.id = "div2";
+
+    let link = document.createElement("img");
+    link.src = ele.link;
+    link.id = "link";
+
+    let p = document.createElement("p");
+    p.innerText = ele.p;
+    p.id = "p";
+    div2.append(link, p);
+
+    div.append(img, div1, des, price, div2);
+    container.append(div);
+    div.addEventListener("click", function () {
+      Detail(ele);
+    });
+  });
+}
+Display(Product_data);
+
+let contain = document.getElementById("contain");
+
+function Detail(el) {
+  let div_p = document.createElement("div");
+
+  let img1 = document.createElement("img");
+  img1.src = el.image;
+
+  let div1_p = document.createElement("div");
+  div1_p.id = "div1";
+
+  let div = document.createElement("div");
+  div.id = "prod_div";
+
+  let img = document.createElement("img");
+  img.src = ele.image;
+  img.id = "picture";
+
+  let div1 = document.createElement("div");
+  div1.id = "div1";
+  let name = document.createElement("h4");
+  name.innerText = ele.name;
+  name.id = "name";
+  let like = document.createElement("img");
+  like.src = ele.like;
+  like.id = "like";
+  div1.append(name, like);
+
+  let des = document.createElement("p");
+  des.innerText = ele.des;
+  let price = document.createElement("h4");
+  price.innerHTML = ele.price;
+
+  let div2 = document.createElement("div");
+  div2.id = "div2";
+
+  let link = document.createElement("img");
+  link.src = ele.link;
+  link.id = "link";
+
+  let p = document.createElement("p");
+  p.innerText = ele.p;
+  p.id = "p";
+  div2.append(link, p);
+
+  div_p.append(img1, div1_p, des, price, div2);
+  contain.append(div_p);
+}
