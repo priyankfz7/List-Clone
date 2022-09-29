a9aab328b4c12b9b93e951006d68fa6d835ca0aa
kripa Shankar Pandey <kripapandey302@gmail.com>
2022-09-29 17:53:06 +0530

    DONE

---
+++ kripa/components/footer.js
@@ -0,0 +1,50 @@
+const footer=()=>{
+return`   <div id="footer_part1">
+<img
+  src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg-hcu0rjvXaE0Ac1WJtW2ts5wRX9Yq444a31vKAx30qxCG7aCbDF1VW5hAOO004JEzbnmIpSwhBYyqjoFQBikc66vSastmek1uAxsTF6Wf5eeZfUz_EsifbaMlhbc1Tsf5m8TLU4D9XvLQhoQgmkmlfzejuYcX_jJQApwEVZBfIhP89VLJ7-xLANQs/s320/Screenshot%202022-09-29%20121925.png"
+  alt="certificate"
+/>
+<span>
+  &nbsp &nbsp
+  <a href="#"
+    >Learn about the Lyst app for iPhone, iPad and Android.
+  </a></span
+>
+<span id="copyright"> &nbsp &nbsp © 2022 Lyst</span>
+</div>
+<div id="footer_part2">
+<h3>INTERNATIONAL</h3>
+<a href="#">Lyst - AU</a>
+<a href="#">Lyst - CA</a>
+<a href="#">Lyst - UK</a>
+<a href="#">Lyst - Österreich</a>
+<a href="#">Lyst - Schweiz</a>
+<a href="#">Lyst - Deutschland</a>
+<a href="#">Lyst - España</a>
+<a href="#">Lyst - France</a>
+<a href="#">Lyst - Italia</a>
+<a href="#">Lyst - 日本</a>
+<a href="#">Lyst - België</a>
+<a href="#">Lyst - Nederland</a>
+</div>
+<div id="footer_part3">
+<h3>HELP AND INFO</h3>
+<a href="#">Help center</a>
+<a href="#">About us</a>
+<a href="#">Shipping policy</a>
+<a href="#">Returns policy</a>
+<a href="#">Payments</a>
+<a href="#">Refund policy</a>
+<a href="#">Developers</a>
+<a href="#">Careers</a>
+<a href="#">Contact</a>
+<a href="#">Terms & conditions</a>
+<a href="#">Privacy & cookie policy</a>
+<a href="#">Intellectual property</a>
+<a href="#">Categories</a>
+<a href="#">Become a partner</a>
+<a href="#">Lyst Insights</a>
+<a href="#">Lyst News</a>
+</div>`;
+}
+export {footer};
