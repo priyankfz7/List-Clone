d448390d2dfb8233f05ea5d4e65dbdc8fcdf81fc
priyankfz7 <110036030+priyankfz7@users.noreply.github.com>
2022-10-01 18:27:01 +0530

    project

---
+++ priyank/database.js
@@ -0,0 +1,131 @@
+let bag=[
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/cettire/4c5e16c3/isabel-marant-Black-Yenky-Tote-Bag.jpeg",
+        brand:"ISABEL MARANT",
+        name:"Yenky Tote Bag - Black",
+        price:149,
+
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/mytheresa/804bc1d5/gucci-Beige-Schultertasche-Ophidia-GG-Mini.jpeg",
+        brand:"GUCCI",
+        name:"Ophidia GG Mini Shoulder Bag - Natural",
+        price:665,
+        
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/mytheresa/396f8957/cecilie-bahnsen-black-Cabas-Fryd.jpeg",
+        brand:"CECILIE BAHNSEN",
+        name:"Fryd Ruffle-trimmed Tote - Black",
+        price:563,
+        
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/mytheresa/f2344880/saint-laurent-neutrals-Panier-Medium-Raffia-Tote.jpeg",
+        brand:"SAINT LAURENT",
+        name:"Panier Medium Raffia Tote - Multicolor",
+        price:563,
+        
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/matchesfashion/1c60a028/loewe--.jpeg",
+        brand:"LOEWE",
+        name:"Cubi Anagram Jacquard And Leather Shoulder Bag - Natural",
+        price:909,
+        
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/matchesfashion/7b428119/staud-neutrals-Sac-porte-epaule-en-cuir-Moon-small.jpeg",
+        brand:"STAUD",
+        name:"Moon Small Leather Shoulder Bag - Natural",
+        price:278,
+        
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/matchesfashion/04160ffb/wandler--.jpeg",
+        brand:"WANDLER",
+        name:"Penelope Small Leather Shoulder Bag - Black",
+        price:610,
+        
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/net-a-porter/9e999f25/proenza-schouler-white-label-yellow-Watts-Kameratasche-Aus-Leder.jpeg",
+        brand:"PROENZA SCHOULER WHITE LABEL",
+        name:"Watts Leather Camera Bag - Yellow",
+        price:282,
+        
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/mytheresa/47b3f540/saint-laurent-neutrals-Rive-Gauche-Embroidered-Crochet-Tote.jpeg",
+        brand:"SAINT LAURENT",
+        name:"Rive Gauche Raffia-effect Tote Bag - Multicolor",
+        price:1118,
+        
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/matchesfashion/68dfe2d2/staud--.jpeg",
+        brand:"STAUD",
+        name:"Riviera Moon Panelled-leather Shoulder Bag - Blue",
+        price:359,
+        
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/matchesfashion/bdcf3d2d/jw-anderson-green-blue-.jpeg",
+        brand:"JW ANDERSON",
+        name:"Bumper Moon Bi-colour Leather Shoulder Bag - Green",
+        price:482,
+        
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/matchesfashion/1ebb0106/bottega-veneta--.jpeg",
+        brand:"BOTTEGA VENETA",
+        name:"Arco Mini Intrecciato-leather Tote Bag - Natural",
+        price:1861,
+        
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/matchesfashion/79f5796d/anya-hindmarch--.jpeg",
+        brand:"ANYA HINDMARCH",
+        name:"ANYA HINDMARCH",
+        price:1064,
+        
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/mytheresa/c52e9396/staud-multicoloured-Mercato-Raffia-effect-Tote.jpeg",
+        brand:"STAUD",
+        name:"Mercato Raffia-effect Tote - Multicolor",
+        price:267,
+        
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/revolveclothing/5e5f44e2/anine-bing-Tan-Saffron-Bag.jpeg",
+        brand:"ANINE BING",
+        name:"Saffron Bag - Brown",
+        price:238,
+        
+    },
+    {
+        image:"https://cdna.lystit.com/200/250/tr/photos/matchesfashion/91598dc0/jw-anderson--Sac-a-main-en-cuir-tisse-Knot.jpeg",
+        brand:"JW ANDERSON",
+        name:"Knot Woven Leather Handbag - Natural",
+        price:588,
+        
+    },
+    
+
+    
+
+
+]
+let big_pic=[{
+    image:"https://cdna.lystit.com/480/480/r/cms/ea59bbea_bf2d_4441_90aa_b0a1a8ee614b_21dfec2728.png",
+    title:"Bag an Icon"
+},
+{
+    image:"https://cdna.lystit.com/480/480/r/cms/2c09c3f3_34a7_4302_9a44_416fd3fa5d76_10988971ba.png",
+    title:"30 Under 300"
+
+}
+]
+
+export {bag,big_pic}
